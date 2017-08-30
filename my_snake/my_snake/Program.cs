3f0db88ee9da3c7b8365f285bc5b0de9ca09b380
khachkara@gmail.com <khachkara@gmail.com>
2017-08-30 13:39:04 +0300

    Исправил код.

--- my_snake/my_snake/Program.cs
+++ my_snake/my_snake/Program.cs
@@ -12,9 +12,9 @@ namespace my_snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            p1.Draw(p1.x, p1.y, p1.sym);
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            p2.Draw(p2.x, p2.y, p2.sym);
 
             Console.WriteLine(" ");
             Console.WriteLine(" ");
