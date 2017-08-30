b67d3e136082b2ddb701e3d956e9a12a4c6f98fe
khachkara@gmail.com <khachkara@gmail.com>
2017-08-30 15:53:08 +0300

    Пример списков.

--- my_snake/my_snake/Program.cs
+++ my_snake/my_snake/Program.cs
@@ -13,35 +13,31 @@ namespace my_snake
         {
             Point p1 = new Point(1, 3, '*');
             p1.Draw(p1.x, p1.y, p1.sym);
+
             Point p2 = new Point(4, 5, '#');
             p2.Draw(p2.x, p2.y, p2.sym);
 
-            Console.WriteLine(" ");
-            Console.WriteLine(" ");
-            Console.WriteLine("   p1  ---> " + p1.x + "  " + p1.y);
-            Console.WriteLine("   p2  ---> " + p2.x + "  " + p2.y);
+            List<int> numlist = new List<int>();
+            numlist.Add(0);
+            numlist.Add(1);
+            numlist.Add(2);
 
-            p1 = p2; // p2 = p1; <-- у автора
+            int x = numlist[0];
+            int y = numlist[1];
+            int z = numlist[2];
 
-            Console.WriteLine(" ");
-            Console.WriteLine("   p1=p2 p1 ---> " + p1.x + "  " + p1.y);
-            Console.WriteLine("   p1=p2 p2 ---> " + p2.x + "  " + p2.y);
+            foreach (int i in numlist)
+            {
+                Console.WriteLine( i );
+            }
 
-            p2.x = 8; // p1.x = 8; для экперемента можно и эти координаты
-            p2.y = 8; // p2.y = 6; меняет координаты обоих Point
+            numlist.RemoveAt(0);
 
-            Console.WriteLine(" ");
-            Console.WriteLine("   new p2(8,8) p1 ---> " + p1.x + "  " + p1.y);
-            Console.WriteLine("   new p2(8,8) p2 ---> " + p2.x + "  " + p2.y);
-
-            p1.x = 3; // Не важно какие координаты менять х или у 1-ой или 2-ой Point 
-            p2.y = 5; // меняется всё т.к. адрес тот же
-
-            Console.WriteLine(" ");
-            Console.WriteLine("   new p1.x = 3 p1 ---> " + p1.x + "  " + p1.y);
-            Console.WriteLine("   new p2.y = 5 p2 ---> " + p2.x + "  " + p2.y);
-
-            Console.ReadKey();
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            
+            Console.ReadLine();
         }
     }
 }
